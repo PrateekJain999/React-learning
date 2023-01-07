ce82ff03fb0f26e03e809c8e8826b66833121da8
prateekjain9997 <jain22719@gmail.com>
2023-01-07 18:21:22 +0530

    fourth-app

---
+++ fourth-app/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+import Button from '../../UI/Button/Button';
+import './CourseInput.css';
+
+const CourseInput = props => {
+  const [enteredValue, setEnteredValue] = useState('');
+  const [isInputValid, setIsInputValid] = useState(true);
+
+  const goalInputChangeHandler = event => {
+    setEnteredValue(event.target.value);
+    if(!enteredValue.trim()){
+      setIsInputValid(true);
+    }
+  };
+
+  const formSubmitHandler = event => {
+    event.preventDefault();
+    if(!enteredValue.trim()){
+      setIsInputValid(false)
+      return;
+    }
+    props.onAddGoal(enteredValue);
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className={`form-control ${!isInputValid ? 'invalid': ''}`}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </div>
+      <Button type="submit">Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
